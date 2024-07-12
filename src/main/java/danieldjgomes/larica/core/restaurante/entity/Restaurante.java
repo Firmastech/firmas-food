7878c88d77d5ede52a96649d84b3dcf9e6a6cedb
Renan <Renan.Olive.nasc@gmail.com>
2024-07-11 21:11:04 -0300

    Revert "Feature:(Restaurante) adiciona metodo para que valida a entidade Restaurante"
    This reverts commit 7ce07288146bee0124079419ad8573d30f37ee70.

--- src/main/java/danieldjgomes/larica/core/restaurante/entity/Restaurante.java
+++ src/main/java/danieldjgomes/larica/core/restaurante/entity/Restaurante.java
@@ -2,56 +2,70 @@ package danieldjgomes.larica.core.restaurante.entity;
 
 import danieldjgomes.larica.core.cardapio.entity.Cardapio;
 import danieldjgomes.larica.core.endereco.entity.Endereco;
-import danieldjgomes.larica.core.restaurante.entity.enums.StatusFuncionamento;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Restaurante {
 
     private UUID id;
     private String nome;
     private Integer tempoEstimadoDeEntrega;
+
     private StatusFuncionamento statusFuncionamento;
     private Endereco endereco;
     private Cardapio cardapio;
 
-  private void validar(Restaurante restaurante){
-      List<String> atributosInvalidos = new ArrayList<>();
-      validarAtributo(restaurante.getId(),"ID nao pode ser nulo",atributosInvalidos);
-      validarAtributo(restaurante.getNome(),"Nome nao pode ser nulo",atributosInvalidos);
-      validarAtributo(restaurante.getTempoEstimadoDeEntrega(),"Tempo de entrega nao pode ser nulo",atributosInvalidos);
-      validarAtributo(restaurante.getStatusFuncionamento(),"Status de funcionamento nao pode ser nulo",atributosInvalidos);
-      notificar(atributosInvalidos);
-  }
-
-  private void validarAtributo(final Object atributos,
-                                final String message,
-                                List<String> invalidList){
-      try{
-          Objects.requireNonNull(atributos,message);
-      }catch (RuntimeException e){
-          invalidList.add(e.getMessage());
-      }
-  }
-
-  private void notificar(final List<String> atributosInvalidos){
-    if(!atributosInvalidos.isEmpty()){
-       final String message = "Foram encontrado as seguintes inconsistencias: ";
-       final String messagefinal = String.join(" ",atributosInvalidos);
-       throw new RuntimeException(message + messagefinal);
+    public void setId(UUID id) {
+        this.id = id;
     }
-  }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getTempoEstimadoDeEntrega() {
+        return tempoEstimadoDeEntrega;
+    }
+
+    public void setTempoEstimadoDeEntrega(Integer tempoEstimadoDeEntrega) {
+        this.tempoEstimadoDeEntrega = tempoEstimadoDeEntrega;
+    }
+
+    public StatusFuncionamento getStatusFuncionamento() {
+        return statusFuncionamento;
+    }
+
+    public void setStatusFuncionamento(StatusFuncionamento statusFuncionamento) {
+        this.statusFuncionamento = statusFuncionamento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Cardapio getCardapio() {
+        return cardapio;
+    }
+
+    public void setCardapio(Cardapio cardapio) {
+        this.cardapio = cardapio;
+    }
+
+    private enum StatusFuncionamento {
+        ABERTO, FECHADO, INATIVO
+    }
 
 }
--- src/main/resources/sql/addControlColunsAtRestaurant.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE restaurante
-    ADD COLUMN data_inclusao timestamp,
-    ADD COLUMN data_atualizacao timestamp,
-    ADD COLUMN data_exclusao timestamp;
