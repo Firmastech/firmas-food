ddbb4e77383249cf0a64ade3cc9fabdbe9e46a83
Renan <Renan.Olive.nasc@gmail.com>
2024-07-13 10:58:52 -0300

    Feature:(ManipuladorGlobaldeErros) Adiciona um manipulador de erros para enviar erros coerentes ao client side

---
+++ src/main/java/danieldjgomes/larica/core/exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package danieldjgomes.larica.core.exceptions;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
