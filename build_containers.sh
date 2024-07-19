656e01e469a16e71b5bb4e24b942a21d64173caa
danieldjgomes <daniel.djgomes@outlook.com>
2024-07-19 14:43:52 -0300

    arrumando arquivo de backup + README.md

--- build_containers.sh
+++ build_containers.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-# Run docker-compose to start the services in detached mode
+# Remove a stack do Docker
+echo "Removendo a stack 'firmas-food'..."
+docker container rm postgres_firma firmas-food_initdb_1 firmas -f
+
+# Espera para garantir que a stack foi completamente removida
+echo "Esperando 5 segundos para garantir que a stack foi removida..."
+sleep 5
+
+# Remove os volumes
+echo "Removendo os volumes 'firmas-food_postgres_data' e 'firmas-food_keycloak_data'..."
+docker volume rm firmas-food_postgres_data firmas-food_keycloak_data
+
+# Executa o docker-compose para iniciar os serviços em modo destacado
+echo "Iniciando os serviços com docker-compose..."
 docker-compose -f compose.yaml up -d
 
-# Function to check if PostgreSQL is ready
+# Função para verificar se o PostgreSQL está pronto
 wait_for_postgres() {
     until docker exec -t postgres_firma pg_isready -U dbo; do
-        echo "Waiting for PostgreSQL to be ready..."
+        echo "Aguardando o PostgreSQL estar pronto..."
         sleep 1
     done
 }
 
-# Wait for PostgreSQL to be ready
+# Espera até que o PostgreSQL esteja pronto
+echo "Verificando se o PostgreSQL está pronto..."
 wait_for_postgres
 
-# Run the pg_dumpall command once PostgreSQL is ready
+# Executa o comando pg_dumpall quando o PostgreSQL estiver pronto
+echo "Restaurando o dump.sql para o PostgreSQL..."
 cat dump.sql | docker exec -i postgres_firma psql -U postgres
 
-
+echo "Processo concluído."
