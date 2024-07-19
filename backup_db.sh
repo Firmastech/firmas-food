7fda1c649ce7372a7ca3191ded0eb387a5da271d
danieldjgomes <daniel.djgomes@outlook.com>
2024-07-19 14:36:46 -0300

    arrumando arquivo de backup + README.md

--- backup_db.sh
+++ backup_db.sh
@@ -1,4 +1,17 @@
-docker exec -t postgres_firma pg_dumpall -c -U dbo > dump_$(date "+%Y.%m.%d-%H.%M.%S").sql
+#!/bin/bash
+
+# Create the backups directory if it doesn't exist
+mkdir -p backups
+
+# Check if dump.sql exists
+if [ -f "dump.sql" ]; then
+    # Rename the existing dump.sql file with a timestamp
+    mv "dump.sql" "backups/dump_bkp_$(date "+%Y.%m.%d-%H.%M.%S").sql"
+fi
+
+# Execute the pg_dumpall command
+docker exec -t postgres_firma pg_dumpall -c -U dbo > "dump.sql"
+
 
 if [ $? -eq 0 ]; then
     echo "Backup was successful."
